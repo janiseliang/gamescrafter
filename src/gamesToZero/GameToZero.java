a69511f0be684093f3635b088197f85f72bcc3ae
Janise Liang <janise@berkeley.edu>
2020-09-21 16:57:54 -0700

    remoteness added to gamestozero

--- src/gamesToZero/GameToZero.java
+++ src/gamesToZero/GameToZero.java
@@ -1,6 +1,8 @@
 package gamesToZero;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.TreeMap;
 
 /** Parent class of TenGame and TwentyFiveGame. */
 public abstract class GameToZero {
@@ -25,17 +27,32 @@ public abstract class GameToZero {
         return res;
     }
 
-    public int start() {
-        return start;
-    }
-
     public static String primitiveValue(int position) {
         return position == 0 ? "lose" : "not_primitive";
     }
 
     public static void solve(Solver s) {
+        TreeMap<Integer, int[]> res = new TreeMap<>();
+
+        int[] totals = new int[] {0, 0, 0, 0};
+
         for (int i = start; i >= 0; i -= 1) {
-            System.out.println(String.format("%02d: ", i)+ s.solve(i));
+            Solver.State x = s.solve(i);
+            if (!res.containsKey(x.rem)) {
+                res.put(x.rem, new int[]{0, 0, 0});
+            }
+            int j = x.win ? 0 : (x.lose ? 1 : 2);
+            res.get(x.rem)[j] += 1;
+            totals[j]++; totals[3]++;
         }
+
+        System.out.println("Remote  Win   Lose   Tie    Total\n---------------------------------");
+
+        for (int i : res.descendingKeySet()) {
+            int[] x = res.get(i);
+            System.out.printf("%-3d     %-3d   %-3d    %-3d    %-3d\n", i, x[0], x[1], x[2], x[0]+x[1]+x[2]);
+        }
+        System.out.println("---------------------------------");
+        System.out.printf("Total   %-3d   %-3d    %-3d    %-3d\n", totals[0], totals[1], totals[2], totals[3]);
     }
 }
--- src/gamesToZero/Solver.java
+++ src/gamesToZero/Solver.java
@@ -7,40 +7,64 @@ package gamesToZero;
 public class Solver {
 
     GameToZero game;
-    String[] cache;
+    State[] cache;
 
     public Solver(GameToZero g) {
         game = g;
-        cache = new String[g.start() + 1];
+        cache = new State[GameToZero.start + 1];
     }
 
-    public String solve(int position) {
+    public State solve(int position) {
         if (cache[position] != null) {
             return cache[position];
         }
         String state = GameToZero.primitiveValue(position);
         if (!state.equals("not_primitive")) {
-            return setCache(position, state);
+            return setCache(position, state, 0);
         }
         int[] moves = game.generateMoves(position);
-        String[] future = new String[moves.length];
+        State[] future = new State[moves.length];
         for (int i = 0; i < moves.length; i++) {
             int newPos = GameToZero.doMove(position, moves[i]);
             future[i] = solve(newPos);
         }
-        boolean flag = true;
-        for (String f : future) {
-            if (f.equals("lose")) {
-                return setCache(position, "win");
-            } else {
-                flag = flag && f.equals("win");
+
+        char val = 'l'; // l = lose (all winning children)
+                        // w = win (one losing child)
+                        // t = tie (1+ tie, rest winning)
+        int remoteness = future[0].rem;
+        for (State child : future) {
+            if (child.lose && val == 'l') {
+                //first losing child
+                val = 'w';
+                remoteness = child.rem;
+            } else if (child.tie && val == 'l') {
+                //first tie, all prev winning children
+                val = 't';
+                remoteness = child.rem;
+            } else if ((child.win && val == 'l') || (child.lose && val == 'w')
+                    || (child.tie && val == 't')) {
+                remoteness = Math.min(remoteness, child.rem);
             }
         }
-        return setCache(position, flag ? "lose" : "tie");
+        remoteness += 1;
+        String v = val == 'l' ? "lose" : (val == 'w' ? "win" : "tie");
+        return setCache(position, v, remoteness);
     }
 
-    String setCache(int pos, String val) {
-        cache[pos] = val;
-        return val;
+    public State setCache(int position, String state, int remoteness) {
+        this.cache[position] = new State(state, remoteness);
+        return this.cache[position];
+    }
+
+    public static class State {
+        boolean win, lose, tie;
+        int rem;
+        public State(String state, int remoteness) {
+            this.rem = remoteness;
+            win = state.equals("win");
+            lose = !win && state.equals("lose");
+            tie = !win && !lose;
+        }
     }
 }
